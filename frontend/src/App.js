ff40608a1307f1abe9eafde7b0e3494998c1e1d9
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-12-05 19:35:06 -0500

    Completed signup component and added redirection to login page for the user upon sucessfull sign up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
 import SignUp from "./Components/SignUp";
+import Login from "./Components/Login";
+import Home from "./Components/Home";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="root-container">
-      <SignUp />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/sign-up" Component={SignUp} />
+        <Route path="/login" Component={Login} />
+      </Routes>
     </div>
   );
 };
